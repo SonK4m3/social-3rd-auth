dc15d08b46ac209f211b3995fd5414411833f64a
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2024-03-19 18:34:26 +0700

    refactor: Update signup logic, oauth2Google in auth controller

--- server/auth-server/src/controller/auth.controller.ts
+++ server/auth-server/src/controller/auth.controller.ts
@@ -32,13 +32,17 @@ export default class AuthController {
 		const body = req.body as CreateUserInput;
 		try {
 			const existingUser = await userService.findUserByEmail(body.email);
-			if (!existingUser)
+			if (existingUser)
 				return res.status(404).json({
-					message: 'User already exists!',
+					message: 'User has existing!',
 				});
 
-			const newUser = (await userService.createUser(body)) as UserResponse;
-			res.status(201).send(newUser);
+			const { password, ...rest } = await userService.createUser(body);
+			const jwtToken = generateAccessToken({
+				email: rest.email,
+				id: rest.id,
+			});
+			res.status(201).json({ token: jwtToken });
 		} catch (err) {
 			next(err);
 		}
@@ -71,8 +75,40 @@ export default class AuthController {
 	}
 
 	async oauth2Google(req: Request, res: Response, next: NextFunction) {
+		const userProfile = req.body as GoogleUserInfo;
 		try {
-			const userProfile = req.body as GoogleUserInfo;
+			const existingUser = await userService.findUserByEmail(userProfile.email);
+			let jwtToken = '';
+			if (!existingUser) {
+				const { password, ...rest } = await userService.createUser({
+					email: userProfile.email,
+					password: userProfile.aud,
+					name: userProfile.name,
+					role: 'user',
+				});
+				jwtToken = generateAccessToken({
+					email: rest.email,
+					id: rest.id,
+				});
+			} else {
+				jwtToken = generateAccessToken({
+					email: existingUser.email,
+					id: existingUser.id,
+				});
+			}
+			if (jwtToken === '')
+				return res.status(400).json({
+					message: 'Userinfo is incorrect',
+				});
+			res.status(200).json({ token: jwtToken });
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	async oauth2Facebook(req: Request, res: Response, next: NextFunction) {
+		try {
+			const userProfile = req.body;
 
 			const jwtToken = generateAccessToken({ id: userProfile.aud, email: userProfile.email });
 			res.status(200).json({ token: jwtToken });
@@ -80,6 +116,4 @@ export default class AuthController {
 			next(err);
 		}
 	}
-
-	async oauth2Facebook() {}
 }
